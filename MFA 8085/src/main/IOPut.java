ec2ed7bd99e6e88d1ef9787f1ce99bdd767ea4be
faalden <faalden@web.de>
2014-11-11 22:36:51 +0100

    GUI splitt in more supclass

--- MFA 8085/src/main/IOPut.java
+++ MFA 8085/src/main/IOPut.java
@@ -6,46 +6,44 @@ import java.util.Scanner;
 
 import main.gui.GUI;
 
+public class IOPut {
+    File bs = new File("commands.bs");
+    Befehlsdatenbank befehl_DB;
 
-public class IOPut 
-{
-	File bs =new File("commands.bs");
-	Befehlsdatenbank befehl_DB;
-	public IOPut(Befehlsdatenbank befehlDB)
-	{
-		this.befehl_DB=befehlDB;
-		this.readfile();
+    public IOPut(Befehlsdatenbank befehlDB) {
+	this.befehl_DB = befehlDB;
+	this.readfile();
+    }
+
+    public void readfile() {
+	Scanner sc = null;
+	String line, sp[], sp2[] = new String[2];
+	byte sp3[] = new byte[2], sp4[] = new byte[2];
+	try {
+	    sc = new Scanner(this.bs);
+	} catch (FileNotFoundException e) {
+	    System.out.println("commands.bs nicht gefunden");
 	}
-	public void readfile()
-	{
-		Scanner sc =null;
-		String line,sp[],sp2[]=new String[2];
-		byte sp3[]=new byte[2],sp4[]=new byte[2];
-		try 
-		{
-			sc =new Scanner(this.bs);
-		} catch (FileNotFoundException e) 
-		{
-			System.out.println("commands.bs nicht gefunden");
+	if (sc != null) {
+	    while (sc.hasNextLine()) {
+		line = sc.nextLine();
+		sp = line.split("\\t\\t");
+		sp2 = sp[3].split("/");
+		sp3[0] = Byte.parseByte(sp2[0]);
+		sp3[1] = Byte.parseByte(sp2[1]);
+		sp2 = sp[1].split(",");
+		sp4[0] = Byte.parseByte(sp2[0]);
+		sp4[1] = Byte.parseByte(sp2[1]);
+		sp2 = new String[5];
+		for (byte i = 0; i < 5; i++) {
+		    sp2[i] = "" + sp[5].charAt(i);
 		}
-		if(sc!=null)
-			while(sc.hasNextLine())
-			{
-				line=sc.nextLine();
-				sp=line.split("\\t\\t");
-				sp2=sp[3].split("/");
-				sp3[0]=Byte.parseByte(sp2[0]);
-				sp3[1]=Byte.parseByte(sp2[1]);
-				sp2=sp[1].split(",");
-				sp4[0]=Byte.parseByte(sp2[0]);
-				sp4[1]=Byte.parseByte(sp2[1]);
-				sp2=new String[5];
-				for(byte i=0 ; i<5 ; i++)
-					sp2[i]=""+sp[5].charAt(i);
-				this.befehl_DB.addEntry(Byte.parseByte(sp[4],2), Byte.parseByte(sp[2]) ,sp[0],sp3,sp4,sp2);
-			}
-		else
-			GUI.errormessage("Doesn't found Commands File",true);
+		this.befehl_DB.addEntry(Byte.parseByte(sp[4], 2),
+			Byte.parseByte(sp[2]), sp[0], sp3, sp4, sp2);
+	    }
+	} else {
+	    GUI.createErrorMessage("Doesn't found Commands File", true);
 	}
+    }
 
 }
--- MFA 8085/src/main/Languages.java
+++ MFA 8085/src/main/Languages.java
@@ -3,30 +3,7 @@ package main;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Languages 
-{
-	public static byte langChoice = 0;									//0 = German, 1 = English
-	public static Map<String[],Integer> terms = new HashMap<String[],Integer>();
-	
-	private void selectLanguage()
-	{
-		int i = 0;
-		terms.put(new String[]{"Datei", "File"}, i++);
-		terms.put(new String[]{"Beenden", "Exit"}, i++);
-		terms.put(new String[]{"Extras", "Extras"}, i++);
-		terms.put(new String[]{"Rechner", "Calculator"}, i++);
-		terms.put(new String[]{"Befehlssatz", "Instruction Set"}, i++);
-		terms.put(new String[]{"Hilfe", "Manual"}, i++);
-		terms.put(new String[]{"Ausführen", "Execute"}, i++);
-		terms.put(new String[]{"Aus", "Off"}, i++);
-		terms.put(new String[]{"An", "On"}, i++);
-		terms.put(new String[]{"Schritt-Modus", "Step by Step mode"}, i++);
-		terms.put(new String[]{"Pausenmodus", "Brk mode"}, i++);
-		/*terms.put(new String[]{},i++);
-		terms.put(new String[]{},i++);
-		terms.put(new String[]{},i++);
-		terms.put(new String[]{},i++);
-		terms.put(new String[]{},i++);
-		terms.put(new String[]{},i++);*/
-	}
+public class Languages {
+    public static byte langChoice = 0; // 0 = German, 1 = English
+    public static Map<String[], Integer> terms = new HashMap<String[], Integer>();
 }
